849496eac57c1904b3f4f93a884076deea9c5b58
Neota Moe <neota.moe@gmail.com>
2017-08-16 10:30:26 -0500

    lab 2 completed

---
+++ src/hello/Hello.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import {shallow} from 'enzyme';
+
+import Hello from './Hello';
+
+describe ('Hello World:', function() {
+  it('renders without exploding', () => {
+    expect(shallow(<Hello />)).toHaveLength(1);
+  });
+
+  it('should render with default text', function () {
+    const component = shallow(<Hello />);
+
+    expect (component).toIncludeText('Howdy');
+    expect (component).toIncludeText('Partner');
+  });
+  it('should render with our props', function () {
+    const component = shallow(
+      <Hello friend="Fred"/>
+    );
+
+    expect(component).toIncludeText('Howdy');
+    expect(component).toIncludeText('Fred');
+    expect(component).not.toIncludeText('Partner');
+  });
+
+});
